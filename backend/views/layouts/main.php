9b307c05c30b6fb99294e1147a831f696b7941cb
thangtt 6947 <thangtt6947@co-well.com.vn>
2018-09-25 16:33:02 +0700

    clean view site/index

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -81,74 +81,56 @@ AppAsset::register($this);
                 </li>
             </ul>
         </div>
-        <!-- /.navbar-header -->
-        <!-- /.navbar-top-links -->
-        <!-- /.navbar-static-side -->
     </nav>
-    <!-- End Top Navigation -->
-    <!-- ============================================================== -->
-    <!-- Left Sidebar - style you can find in sidebar.scss  -->
-    <!-- ============================================================== -->
+
     <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav slimscrollsidebar">
             <div class="sidebar-head">
-                <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span>
+                <h3>
+                    <span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span>
+                    <span class="hide-menu">Navigation</span>
                 </h3>
             </div>
             <ul class="nav" id="side-menu">
                 <li style="padding: 70px 0 0;">
-                    <a href="index.html" class="waves-effect"><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i>Dashboard</a>
+                    <a href="/" class="waves-effect"><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i>Dashboard</a>
                 </li>
-                <li>
-                    <a href="profile.html" class="waves-effect"><i class="fa fa-user fa-fw" aria-hidden="true"></i>Profile</a>
-                </li>
-                <li>
-                    <a href="basic-table.html" class="waves-effect"><i class="fa fa-table fa-fw" aria-hidden="true"></i>Basic
-                        Table</a>
-                </li>
-                <li>
-                    <a href="fontawesome.html" class="waves-effect"><i class="fa fa-font fa-fw" aria-hidden="true"></i>Icons</a>
-                </li>
-                <li>
-                    <a href="map-google.html" class="waves-effect"><i class="fa fa-globe fa-fw" aria-hidden="true"></i>Google
-                        Map</a>
-                </li>
-                <li>
-                    <a href="blank.html" class="waves-effect"><i class="fa fa-columns fa-fw" aria-hidden="true"></i>Blank
-                        Page</a>
-                </li>
-                <li>
-                    <a href="404.html" class="waves-effect"><i class="fa fa-info-circle fa-fw" aria-hidden="true"></i>Error
-                        404</a>
-                </li>
-
             </ul>
-            <div class="center p-20">
-                <a href="https://wrappixel.com/templates/ampleadmin/" target="_blank"
-                   class="btn btn-danger btn-block waves-effect waves-light">Upgrade to Pro</a>
-            </div>
         </div>
-
     </div>
+
     <div id="page-wrapper">
         <div class="container-fluid">
             <div class="row bg-title">
                 <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                    <h4 class="page-title">Dashboard</h4></div>
+                    <h4 class="page-title">Dashboard</h4>
+                </div>
                 <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                    <a href="https://wrappixel.com/templates/ampleadmin/" target="_blank"
-                       class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Upgrade to
-                        Pro</a>
+                    <a href="/post/create" target="_blank"
+                       class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Create New
+                        Post</a>
                     <ol class="breadcrumb">
-                        <li><a href="#">Dashboard</a></li>
+                        <li><a href="/system/configuration">Configuration</a></li>
                     </ol>
                 </div>
             </div>
+
+            <div id="main_site">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <?= Breadcrumbs::widget([
+                            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                        ]) ?>
+                        <?= Alert::widget() ?>
+                        <?= $content ?>
+                    </div>
+                </div>
+            </div>
         </div>
+
         <footer class="footer text-center"> <?= Html::encode(Yii::$app->name) ?> &copy; <?= date('Y') ?></footer>
     </div>
 </div>
-
 
 <?php $this->endBody() ?>
 </body>
