1a6f08359eda61d289ad7536ce22a3a6233edaea
thangtt 6947 <thangtt6947@co-well.com.vn>
2018-09-25 16:16:48 +0700

    apply admin theme

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,12 +12,39 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'theme/bootstrap/dist/css/bootstrap.min.css',
+        'theme/plugins/sidebar-nav/dist/sidebar-nav.min.css',
+        'theme/plugins/toast-master/css/jquery.toast.css',
+        'theme/plugins/morrisjs/morris.css',
+        'theme/plugins/chartist-js/dist/chartist.min.css',
+        'theme/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css',
+        'theme/css/animate.css',
+        'theme/css/style.css',
+        'theme/css/colors/default.css'
     ];
     public $js = [
+        'theme/plugins/jquery/dist/jquery.min.js',
+        'theme/bootstrap/dist/js/bootstrap.min.js',
+        'theme/plugins/sidebar-nav/dist/sidebar-nav.min.js',
+        'theme/js/jquery.slimscroll.js',
+        'theme/js/waves.js',
+        'theme/plugins/waypoints/lib/jquery.waypoints.js',
+        'theme/plugins/counterup/jquery.counterup.min.js',
+        'theme/plugins/chartist-js/dist/chartist.min.js',
+        'theme/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js',
+        'theme/plugins/jquery-sparkline/jquery.sparkline.min.js',
+        'theme/js/custom.js',
+        'theme/js/dashboard1.js',
+        'theme/plugins/toast-master/js/jquery.toast.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+//        'yii\web\YiiAsset'
     ];
 }
+
+
+
+
+
+
+
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use backend\assets\AppAsset;
@@ -21,58 +22,133 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => $menuItems,
-    ]);
-    NavBar::end();
-    ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+<body class="fix-header">
+<!-- ============================================================== -->
+<!-- Preloader -->
+<!-- ============================================================== -->
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+    </svg>
+</div>
+<!-- ============================================================== -->
+<!-- Wrapper -->
+<!-- ============================================================== -->
+<div id="wrapper">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <nav class="navbar navbar-default navbar-static-top m-b-0">
+        <div class="navbar-header">
+            <div class="top-left-part">
+                <a class="logo" href="index.html">
+                    <b>
+                        <img src="/images/admin-logo.png" alt="home" class="dark-logo"/>
+                        <img src="/images/admin-logo-dark.png" alt="home" class="light-logo"/>
+                    </b>
+                    <span class="hidden-xs">
+                        <img src="/images/admin-text.png" alt="home" class="dark-logo"/>
+                        <img src="/images/admin-text-dark.png" alt="home" class="light-logo"/>
+                     </span> </a>
+            </div>
+            <ul class="nav navbar-top-links navbar-right pull-right">
+                <li>
+                    <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
+                        <input type="text" placeholder="Search..." class="form-control">
+                        <a href=""><i class="fa fa-search"></i></a>
+                    </form>
+                </li>
+                <li>
+                    <a class="profile-pic" href="#">
+                        <img src="/images/users/varun.jpg" alt="user-img" width="36"
+                             class="img-circle">
+                        <b class="hidden-xs">Steave</b>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-header -->
+        <!-- /.navbar-top-links -->
+        <!-- /.navbar-static-side -->
+    </nav>
+    <!-- End Top Navigation -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav slimscrollsidebar">
+            <div class="sidebar-head">
+                <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span>
+                </h3>
+            </div>
+            <ul class="nav" id="side-menu">
+                <li style="padding: 70px 0 0;">
+                    <a href="index.html" class="waves-effect"><i class="fa fa-clock-o fa-fw" aria-hidden="true"></i>Dashboard</a>
+                </li>
+                <li>
+                    <a href="profile.html" class="waves-effect"><i class="fa fa-user fa-fw" aria-hidden="true"></i>Profile</a>
+                </li>
+                <li>
+                    <a href="basic-table.html" class="waves-effect"><i class="fa fa-table fa-fw" aria-hidden="true"></i>Basic
+                        Table</a>
+                </li>
+                <li>
+                    <a href="fontawesome.html" class="waves-effect"><i class="fa fa-font fa-fw" aria-hidden="true"></i>Icons</a>
+                </li>
+                <li>
+                    <a href="map-google.html" class="waves-effect"><i class="fa fa-globe fa-fw" aria-hidden="true"></i>Google
+                        Map</a>
+                </li>
+                <li>
+                    <a href="blank.html" class="waves-effect"><i class="fa fa-columns fa-fw" aria-hidden="true"></i>Blank
+                        Page</a>
+                </li>
+                <li>
+                    <a href="404.html" class="waves-effect"><i class="fa fa-info-circle fa-fw" aria-hidden="true"></i>Error
+                        404</a>
+                </li>
+
+            </ul>
+            <div class="center p-20">
+                <a href="https://wrappixel.com/templates/ampleadmin/" target="_blank"
+                   class="btn btn-danger btn-block waves-effect waves-light">Upgrade to Pro</a>
+            </div>
+        </div>
+
+    </div>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row bg-title">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                    <h4 class="page-title">Dashboard</h4></div>
+                <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                    <a href="https://wrappixel.com/templates/ampleadmin/" target="_blank"
+                       class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Upgrade to
+                        Pro</a>
+                    <ol class="breadcrumb">
+                        <li><a href="#">Dashboard</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <footer class="footer text-center"> <?= Html::encode(Yii::$app->name) ?> &copy; <?= date('Y') ?></footer>
     </div>
 </div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
